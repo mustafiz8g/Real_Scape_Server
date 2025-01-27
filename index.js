8f953e490002d127dc41d5420afbace3ed6212c1
mustafiz8g <mustafizur8g@gmail.com>
2025-01-27 13:05:56 +0600

    create user related api

--- index.js
+++ index.js
@@ -55,24 +55,66 @@ async function run() {
     const usersCollection = db.collection('users')
     const propertiesCollection = db.collection('properties')
 
-        // save or update user in db
-        app.post('/users/:email', async(req, res) => {
-            const email = req.params.email;
-            const query = { email }
-            const user = req.body;
-            // check if user exist in deb
-            const isExist = await usersCollection.findOne(query)
-            if(isExist){
-              return res.send(isExist)
-            }
-      
-            const result = await usersCollection.insertOne({
-              ...user,
-              role: 'customer',
-              timestamp: Date.now(),
-              })
-            res.send(result)
+
+      // save or update user in db
+      app.post('/users/:email', async(req, res) => {
+        const email = req.params.email;
+        const query = { email }
+        const user = req.body;
+        // check if user exist in deb
+        const isExist = await usersCollection.findOne(query)
+        if(isExist){
+          return res.send(isExist)
+        }
+  
+        const result = await usersCollection.insertOne({
+          ...user,
+          role: 'customer',
+          timestamp: Date.now(),
           })
+        res.send(result)
+      })
+
+
+            app.patch('/users/:email',verifyToken, async(req, res) => { 
+      const email = req.params.email;
+      const query = { email }
+      const user = await usersCollection.findOne(query)
+      if(!user || user?.status === 'requested') return res.status(400 ).send({message: 'already requested wait some time'})
+      // const {status} = req.body;
+      const updateDoc = {
+        $set: {
+          status: 'requested',
+        },
+      }
+      const result = await usersCollection.updateOne(query, updateDoc)
+      res.send(result)
+    })
+
+
+    app.patch('/users/:email',verifyToken, async(req, res) => { 
+        const email = req.params.email;
+        const query = { email }
+        const user = await usersCollection.findOne(query)
+        if(!user || user?.status === 'requested') return res.status(400 ).send({message: 'already requested wait some time'})
+        // const {status} = req.body;
+        const updateDoc = {
+          $set: {
+            status: 'requested',
+          },
+        }
+        const result = await usersCollection.updateOne(query, updateDoc)
+        res.send(result)
+      })
+  
+          //get user role
+    app.get('/users/role/:email',verifyToken, async(req, res) => {
+        const email = req.params.email;
+        const query = { email }
+        const result = await usersCollection.findOne(query)
+        res.send({role: result?.role})
+      })
+  
 
     // Generate jwt token
     app.post('/jwt',verifyToken, async (req, res) => {
